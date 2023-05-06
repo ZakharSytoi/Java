3085d4632fbdf83926b0d54e35815d4f231495dd
ZakharSytoi <zakharsytoi@gmail.com>
2023-05-06 16:50:40 +0200

    LeetCode: task 15 my better uneffective solution

--- Leetcode/S15/src/main/java/org/example/Solution.java
+++ Leetcode/S15/src/main/java/org/example/Solution.java
@@ -1,44 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        boolean flag2 = true;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length - 1; j++) {
-                for (int k = 0; k < nums.length; k++) {
-                    if(i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0){
-                        List<Integer> tmp = new ArrayList<Integer>(3);
-                        tmp.add(nums[i]);
-                        tmp.add(nums[j]);
-                        tmp.add(nums[k]);
-
-                        if(nums[i] == 0 && nums[j] == 0 && nums[k] == 0 && flag2) {
-                            resultList.add(tmp);
-                            flag2 = false;
-                            continue;
-                        }
-                        boolean flag = false;
-                        for (List<Integer> instance:
-                                resultList) {
-                            if(new HashSet<>(instance).containsAll(tmp)){
-                                flag = true;
-                                break;
-                            }
-                        }
-                        if (!flag) {
-                            resultList.add(tmp);
-                        }
-
+        HashSet<List<Integer>> resultSet = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> tmp = new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
+                        Collections.sort(tmp);
+                        resultSet.add(tmp);
                     }
                 }
             }
         }
-        return resultList;
+        return new ArrayList<>(resultSet);
     }
 }
-
