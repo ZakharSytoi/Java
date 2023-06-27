6ed20122b005e26445306c396a69e877d26cc577
ZakharSytoi <zakharsytoi@gmail.com>
2023-06-27 14:14:32 +0200

    LC-task 121: my solution, but i had the hint about plot already

--- Leetcode/S121/src/main/java/org/example/App.java
+++ Leetcode/S121/src/main/java/org/example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         Solution s = new Solution();
-        s.maxProfit(new int[]{7,1,5,3,6,4});
+        System.out.println(s.maxProfit(new int[]{7,1,5,3,6,4}));
     }
 }
--- Leetcode/S121/src/main/java/org/example/Solution.java
+++ Leetcode/S121/src/main/java/org/example/Solution.java
@@ -1,44 +1,19 @@
 package org.example;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) {
-            return 0;
-        }
-        int actualIndexOfMinElement;
-        int maxLocalElement;
-        ArrayList<Integer> tmpList = new ArrayList<Integer>();
         int maxProfit = 0;
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        for (int price : prices) {
-            list.add(price);
-        }
-
-        Arrays.sort(prices);
-
-        for (int minPrice : prices) {
-            if ((actualIndexOfMinElement = list.indexOf(minPrice)) == list.size() - 1) {
-                return maxProfit;
+        int minPrice = prices[0];
+        int maxPrice = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if(minPrice > prices[i]){
+                minPrice = prices[i];
+                maxPrice = 0;
+                continue;
             }
-            else{
-                maxLocalElement = 0;
-                for(int i = actualIndexOfMinElement+1; i < list.size(); i++){
-                    tmpList.add(list.get(i));
-                }
-                for (Integer integer : tmpList) {
-                    if (integer > maxLocalElement) maxLocalElement = integer;
-                }
-                if(maxProfit < maxLocalElement - minPrice){
-                    maxProfit = maxLocalElement - minPrice;
-                }
-                list.remove(actualIndexOfMinElement);
-                tmpList.clear();
-            }
+            maxPrice = Math.max(maxPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, maxPrice - minPrice);
         }
-
         return maxProfit;
     }
 }
