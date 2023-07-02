c0f0a9335a3160cad487319793bcd2ae087c8078
ZakharSytoi <zakharsytoi@gmail.com>
2023-07-02 10:27:55 +0200

    LC task 167: first try perfect solution

---
+++ Leetcode/S167/src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Solution s = new Solution();
+        s.twoSum(new int[]{-1,0}, -1);
+    }
+}
