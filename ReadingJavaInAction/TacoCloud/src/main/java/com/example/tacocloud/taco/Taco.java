0233993638155e3977f88f6d1c2e227df844ecc4
ZakharSytoi <zakharsytoi@gmail.com>
2023-08-01 09:47:31 +0200

    validation annotations added to Taco and TacoOrder

--- ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/taco/Taco.java
+++ ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/taco/Taco.java
@@ -1,12 +1,18 @@
 package com.example.tacocloud.taco;
 
 import com.example.tacocloud.ingredient.Ingredient;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, max = 64, message = "Name must be from 5 to 64 characters long.")
     private String name;
+    @NotNull
+    @Size(min = 2, message = "Choose at least 2 ingredients please.")
     private List<Ingredient> ingredients;
 }
--- ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/tacoOrder/TacoOrder.java
+++ ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/tacoOrder/TacoOrder.java
@@ -1,21 +1,34 @@
 package com.example.tacocloud.tacoOrder;
 
 import com.example.tacocloud.taco.Taco;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
+    @NotBlank
     private String deliveryName;
+    @NotBlank
     private String deliveryStreet;
+    @NotBlank
     private String deliveryCity;
+    @NotBlank
     private String deliveryState;
+    @NotBlank
     private String deliveryZip;
+    @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+            message="Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+    @NotNull
+    @Size(min = 1, message = "Create at least one taco please.")
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
