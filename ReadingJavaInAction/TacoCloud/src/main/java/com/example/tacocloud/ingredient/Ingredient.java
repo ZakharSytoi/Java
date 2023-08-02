af7aa587a3343fbd68a71d70b38a38ca445b1e91
ZakharSytoi <zakharsytoi@gmail.com>
2023-08-02 12:36:45 +0200

    ingredient now Entity

--- ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/ingredient/Ingredient.java
+++ ReadingJavaInAction/TacoCloud/src/main/java/com/example/tacocloud/ingredient/Ingredient.java
@@ -1,11 +1,17 @@
 package com.example.tacocloud.ingredient;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
+    @Id
     private String id;
     private String name;
     private IngredientType type;
